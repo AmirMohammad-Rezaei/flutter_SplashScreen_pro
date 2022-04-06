7e473e33bcf6af684edc9504367dc24b922d6a6f
AmirMohammad_Rezaei <amirrezaei1133@gmail.com>
2022-04-06 18:14:21 +0430

    perpose page added

--- lib/Splash_ScreenPage.dart
+++ lib/Splash_ScreenPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Splash_ScreenPage extends StatefulWidget {
-  const Splash_ScreenPage({this.child, this.backgroundcolor, Key? key})
+  const Splash_ScreenPage(
+      {this.child, required this.purpose_Page, this.backgroundcolor, Key? key})
       : super(key: key);
   final Widget? child;
   final Color? backgroundcolor;
+  final Widget purpose_Page;
 
   @override
   State<Splash_ScreenPage> createState() => _Splash_ScreenPageState();
@@ -12,19 +14,32 @@ class Splash_ScreenPage extends StatefulWidget {
 
 class _Splash_ScreenPageState extends State<Splash_ScreenPage> {
   @override
+  void initState() {
+    super.initState();
+    GoMainPage(context);
+  }
+
+  Future GoMainPage(context) async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => widget.purpose_Page));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Container(
-            color: widget.backgroundcolor ?? Colors.white,
-            child: Center(
-              child: widget.child ??
-                  const Icon(
-                    Icons.facebook,
-                    color: Colors.amber,
-                    size: 30,
-                  ),
-            )),
+          color: widget.backgroundcolor ?? Colors.white,
+          child: Center(
+            child: widget.child ??
+                const Icon(
+                  Icons.facebook,
+                  color: Colors.amber,
+                  size: 30,
+                ),
+          ),
+        ),
       ),
     );
   }
